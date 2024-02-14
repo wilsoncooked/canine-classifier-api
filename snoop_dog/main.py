3e8fa4740a091585d7eba912e8a43077444f8e93
Marcel Lommerzheim <mlommerzheim@gmx.de>
2024-02-14 17:09:19 +0100

    Send picture b64 encoded in JSON

--- snoop_dog/main.py
+++ snoop_dog/main.py
@@ -1,8 +1,9 @@
 from snoop_dog.predict import format_predictions
 from snoop_dog.class_names import class_names
-from snoop_dog.registry import load_image, dogimg_process
+from snoop_dog.registry import load_image, dogimg_process, gradcam
+import base64
 
-
+    
 
 # put it all together for the api
 def predict_breeds(model, img):
@@ -11,4 +12,6 @@ def predict_breeds(model, img):
     model = model
     predictions = model.predict(image_processed)
     output = format_predictions(predictions, class_names)
+    grad_img = gradcam(model, image_processed)
+    output[0]['ai_view'] = base64.b64encode(grad_img) # Check if not a dog
     return output
--- snoop_dog/registry.py
+++ snoop_dog/registry.py
@@ -2,6 +2,10 @@ from tensorflow import keras
 import numpy as np
 from PIL import Image
 import os
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
+import io
 
 def load_image(img):
     img = Image.open(img).convert("RGB")
@@ -18,3 +22,42 @@ def dogimg_process(img):
 def load_dogmodel():
     new_model = keras.models.load_model(f"{os.path.dirname(os.path.dirname(__file__))}/models/effnetv2s_finetuned_valsplit01.keras")
     return new_model
+
+def gradcam(model, img_array):
+    last_conv_layer = model.get_layer('efficientnetv2-s').get_layer('top_activation')
+    last_conv_layer_model = tf.keras.Model(model.get_layer('efficientnetv2-s').inputs, last_conv_layer.output)
+    
+    classifier_input = tf.keras.Input(shape=last_conv_layer.output.shape[1:])
+    x = classifier_input
+    for layer_name in ["global_average_pooling2d", "dense"]:
+        x = model.get_layer(layer_name)(x)
+    classifier_model = tf.keras.Model(classifier_input, x)
+    
+    with tf.GradientTape() as tape:
+        inputs = img_array
+        last_conv_layer_output = last_conv_layer_model(inputs)
+        tape.watch(last_conv_layer_output)
+        preds = classifier_model(last_conv_layer_output)
+        top_pred_index = tf.argmax(preds[0])
+        top_class_channel = preds[:, top_pred_index]
+    
+    grads = tape.gradient(top_class_channel, last_conv_layer_output)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    last_conv_layer_output = last_conv_layer_output.numpy()[0]
+    pooled_grads = pooled_grads.numpy()
+    for i in range(pooled_grads.shape[-1]):
+        last_conv_layer_output[:, :, i] *= pooled_grads[i]
+    np.shape(last_conv_layer_output)
+    gradcam = np.mean(last_conv_layer_output, axis=-1)
+    gradcam = np.clip(gradcam, 0, np.max(gradcam)) / np.max(gradcam)
+    gradcam = cv2.resize(gradcam, (256, 256)) # Can be probably also be done with PIL
+    fig = plt.figure(visible=False)
+    canvas = fig.canvas
+    ax = fig.gca()
+    fig = plt.imshow(np.squeeze(img_array))
+    fig = plt.imshow(gradcam, alpha=0.5)
+    ax.axis('off')
+    canvas.draw()
+    image_flat = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
+    img_arr = image_flat.reshape(*reversed(canvas.get_width_height()), 3)
+    return img_arr
